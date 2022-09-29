0ed0fea6b28fa2c088ed98f50b4af89351ad6a98
xiaolajinumberlaatnow <112558178+xiaolajinumberlaatnow@users.noreply.github.com>
2022-09-29 19:27:03 +0800

    Add files via upload

--- src/test/java/com/projectname/hello/CalcTest.java
+++ src/test/java/com/projectname/hello/CalcTest.java
@@ -4,27 +4,10 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-//import static org.junit.Assert.*;
-//
-//import org.junit.Before;
-//import org.junit.Ignore;
-//import org.junit.Test;
 
 public class CalcTest
         extends TestCase
 {
-    
-//    private static Calc calc = new Calc();
-//
-//    @Before
-//    public void setUp() throws Exception {
-//        calc.clear();
-//    }
-//    
-//    public int testCalc(){
-//        assertEquals(3,calc.getResult());
-//        assertEquals(1,calc.getResult());
-//    }
     
    
         /**
@@ -50,53 +33,8 @@ public class CalcTest
          */
         public void testCalc()
         {
-            assertTrue( true );
+            assertTrue( false );
         }
     
 }
 
-//package main;
-//
-//        import static org.junit.Assert.*;
-//
-//        import org.junit.Before;
-//        import org.junit.Ignore;
-//        import org.junit.Test;
-//
-//public class CalculatorTest {
-//
-//    private static Calculator calculator = new Calculator();
-//
-//    @Before
-//    public void setUp() throws Exception {
-//        calculator.clear();
-//    }
-//
-//    @Test
-//    public void testAdd() {
-//        calculator.add(4);
-//        calculator.add(4);
-//        assertEquals(8, calculator.getResult());
-//    }
-//
-//    @Test
-//    public void testSubstract() {
-//        calculator.add(9);
-//        calculator.substract(3);
-//        assertEquals(6, calculator.getResult());
-//    }
-//
-//    @Ignore("Multiply() Not yet implemented")
-//    @Test
-//    public void testMultiply() {
-//        fail("Not yet implemented");
-//    }
-//
-//    @Test
-//    public void testDivide() {
-//        calculator.add(8);
-//        calculator.divide(2);
-//        assertEquals(4, calculator.getResult());
-//    }
-//
-//}
